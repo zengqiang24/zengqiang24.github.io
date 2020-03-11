68e0f67b69258726abb856b796d47d87b9e34cfe
Zeng Qiang <zengqiang11111@gmail.com>
2020-03-11 13:16:55 +0800

    deploy

---
+++ precache-manifest.d472d8d16b9b887a4f35f0a43ba819b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0697bfda04c3fa81a47",
+    "url": "/css/app.d2f43208.css"
+  },
+  {
+    "revision": "7f6387bca87fd75d5621bdb242acd85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b681aaa76f19bc3a6e2",
+    "url": "/js/about.10dfa07a.js"
+  },
+  {
+    "revision": "b0697bfda04c3fa81a47",
+    "url": "/js/app.0a160cbb.js"
+  },
+  {
+    "revision": "962c3712da0b2ef8d5e0",
+    "url": "/js/chunk-vendors.1f9f5a96.js"
+  },
+  {
+    "revision": "306f9e9f6f0e6c6a2ec9f9e5d0b4b4cf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
